9f6a924d3bb7b73ba8a90b49384b2f34adec217d
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-10-14 17:01:06 -0300

    Added env settings for production vs dev

---
+++ app.config.js
@@ -0,0 +1,7 @@
+export default {
+    name: 'UbademyApp',
+    version: '1.0.0',
+    extra: {
+      api_dev_url: "http://localhost:8516",
+    },
+};
--- src/scripts/logIn.ts
+++ src/scripts/logIn.ts
@@ -1,14 +1,17 @@
+import Constants from 'expo-constants';
+
 export default async function sendLoginCredentials(username: string, password: string) {
     const loginData = JSON.stringify({
-        'username': username,
+        'email': username,
         'password': password
     });
-    await fetch('https://localhost:8080', {
+    console.log(Constants.manifest.extra.api_dev_url);
+    await fetch(Constants.manifest.extra.api_dev_url + '/login/', {
         method: 'POST',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json'
         },
         body: loginData
-    }).catch(() => console.log("Could not connect to the server"));
+    }).then((response) => console.log(response)).catch(() => console.log("Could not connect to the server"));
 }
