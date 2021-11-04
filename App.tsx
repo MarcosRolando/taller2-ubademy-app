bc6bc4e7f335b2da92d7a54a9df0cfc17ab15fc1
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-11-03 22:02:33 -0300

    Fix react navigator library bug with white animation on transitions

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignupScreen, LocationScreen, 
   CoursesScreen as RegisterCoursesScreen, LoginScreen } from './src/components/register/Screens';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import colors from './src/constants/colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,16 +16,18 @@ export default function App() {
   return (
     <PaperProvider theme={UbademyTheme}>
       <StatusBar style='light' />
-      <NavigationContainer theme={NavigationTheme}>
-        <Stack.Navigator screenOptions={{headerShown:false}}>
-          <Stack.Group screenOptions={{presentation:'card'}}>
-            <Stack.Screen name='Login' component={LoginScreen} />
-            <Stack.Screen name='Signup' component={SignupScreen} />
-            <Stack.Screen name='Location' component={LocationScreen} />
-            <Stack.Screen name='RegisterCourses' component={RegisterCoursesScreen} />
-          </Stack.Group>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider style={{ backgroundColor: colors.background}}> #This is because React Navigator is a piece of shit and gives me a fucking siezure with a white animation bug and this was the only stack overflow solution that worked. Fuck you React Navigator.
+        <NavigationContainer theme={NavigationTheme}>
+          <Stack.Navigator screenOptions={{headerShown:false}}>
+            <Stack.Group>
+              <Stack.Screen name='Login' component={LoginScreen} />
+              <Stack.Screen name='Signup' component={SignupScreen} />
+              <Stack.Screen name='Location' component={LocationScreen} />
+              <Stack.Screen name='RegisterCourses' component={RegisterCoursesScreen} />
+            </Stack.Group>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </PaperProvider>
   );
 }
