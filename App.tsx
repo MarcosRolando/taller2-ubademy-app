253acc86e1943b65d18d8492a96f5ed8c1fb54dc
xNicky <nraveszani@gmail.com>
2021-11-20 20:39:48 -0300

    renombro course a courseView

--- App.tsx
+++ App.tsx
@@ -12,43 +12,24 @@ import {UbademyTheme, NavigationTheme} from './src/styling/themes';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Root from './src/components/Root';
-import Course from './src/components/course/Course';
+import Course from './src/components/courseView/Course';
 
 const Stack = createNativeStackNavigator();
 
 
-// export default function App() {
-//   return (
-//     <PaperProvider theme={UbademyTheme}>
-//       <StatusBar style='light' />
-//       <SafeAreaProvider style={{backgroundColor: colors.background}}>
-//         <NavigationContainer theme={NavigationTheme}>
-//           <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={LOGIN}>
-//             <Stack.Group>
-//               <Stack.Screen name={LOGIN} component={LoginScreen} />
-//               <Stack.Screen name={SIGNUP} component={SignupScreen} />
-//               <Stack.Screen name={PROFILE_SETUP} component={ProfileSetupScreen} />
-//             </Stack.Group>
-//             <Stack.Screen name={ROOT} component={Root} />
-//           </Stack.Navigator>
-//         </NavigationContainer>
-//       </SafeAreaProvider>
-//     </PaperProvider>
-//   );
-// }
-
-
-
 export default function App() {
-  
   return (
     <PaperProvider theme={UbademyTheme}>
       <StatusBar style='light' />
-
       <SafeAreaProvider style={{backgroundColor: colors.background}}>
         <NavigationContainer theme={NavigationTheme}>
           <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={LOGIN}>
-            <Stack.Screen name ={COURSE} component={Course} />
+            <Stack.Group>
+              <Stack.Screen name={LOGIN} component={LoginScreen} />
+              <Stack.Screen name={SIGNUP} component={SignupScreen} />
+              <Stack.Screen name={PROFILE_SETUP} component={ProfileSetupScreen} />
+            </Stack.Group>
+            <Stack.Screen name={ROOT} component={Root} />
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
