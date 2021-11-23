4b019056132be95d4dce45aa7659381f410ec84f
xNicky <nraveszani@gmail.com>
2021-11-23 17:46:24 -0300

    Agrego parte de la comunicación con updater curso

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -7,3 +7,4 @@ export const CREATE_COURSE = 'courses/create_course';
 export const COURSE_SETUP = 'course_setup';
 export const PROFILE = 'profile';
 export const UPDATE_PROFILE = 'update_profile';
+export const UPDATE_COURSE = 'courses/update_course';
