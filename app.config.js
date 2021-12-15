b276c0a3a6e13f287190c079ca796ae165db5b4d
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-12-15 18:16:13 -0300

    Add google ids for standalone google signin

--- app.config.js
+++ app.config.js
@@ -5,6 +5,12 @@ export default {
       API_URL: (process.env.PROD != undefined) ? "https://ubademy-api-gate.herokuapp.com/" : "http://192.168.0.68:8516/",
     },
     android: {
-      package: "ubademy.apk"
+      package: "ubademy.apk",
+      config: {
+        googleSignIn: {
+          apiKey: AIzaSyDwz6kInmqnhRQTqcvgU7Xg7z7B9DJX2Fc,
+          certificateHash: BD03E8B521F1D7589AB02CE5B6DE04DB72C8B759
+        }
+      }
     }
 };
