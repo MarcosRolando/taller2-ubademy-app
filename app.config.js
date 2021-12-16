71f2e6679e27f8a335174651ec6f7eb2aa752dc9
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-12-16 17:21:35 -0300

    Google deploy works

--- app.config.js
+++ app.config.js
@@ -2,14 +2,14 @@ export default {
     name: 'UbademyApp',
     version: '1.0.0',
     extra: {
-      API_URL: (process.env.PROD != undefined) ? "https://ubademy-api-gate.herokuapp.com/" : "http://192.168.0.68:8516/",
+      API_URL: "https://ubademy-api-gate.herokuapp.com/",
     },
     android: {
       package: "ubademy.apk",
       config: {
         googleSignIn: {
-          apiKey: AIzaSyDwz6kInmqnhRQTqcvgU7Xg7z7B9DJX2Fc,
-          certificateHash: BD03E8B521F1D7589AB02CE5B6DE04DB72C8B759
+          apiKey: 'AIzaSyDwz6kInmqnhRQTqcvgU7Xg7z7B9DJX2Fc',
+          certificateHash: 'BD03E8B521F1D7589AB02CE5B6DE04DB72C8B759'
         }
       }
     }
